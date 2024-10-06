2bbf357e341aada053ff7f7cc2a4b102c5858520
DarkNight7593 <juan.inca@utec.edu.pe>
2024-10-06 18:22:49 -0500

    trabajo

--- AgendarCita/routes/citas.js
+++ AgendarCita/routes/citas.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const axios = require('axios');
 
 // URLs de los microservicios
-const PACIENTE_SERVICE_URL = 'http://localhost:8000/pacientes';
+const PACIENTE_SERVICE_URL = 'http://localhost:8082/pacientes';
 const DOCTOR_SERVICE_URL = 'http://localhost:3000/doctors';
 const DISPONIBILIDAD_SERVICE_URL = 'http://localhost:3000/disponibilidad';
 const CITA_SERVICE_URL = 'http://localhost:8080/citas';
