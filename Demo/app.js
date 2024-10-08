bd23d99ca312b8db23352dabbc12755a7035df5d
DarkNight7593 <165918596+DarkNight7593@users.noreply.github.com>
2024-10-07 21:09:46 -0500

    Update app.js

--- Demo/app.js
+++ Demo/app.js
@@ -24,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json());
 
 // Rutas
+app.use('/', indexRouter);
 app.use('/doctors', doctorRouter);
 app.use('/disponibilidad', disponibilidadRouter);
 
